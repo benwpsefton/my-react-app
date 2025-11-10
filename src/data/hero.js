c3d542a0904610a8ea6605e334e900ce0e7cb35d
ben sefton <benwpsefton@gmail.com>
2025-11-10 21:33:29 +0800

    made hero data unique

--- src/data/hero.js
+++ src/data/hero.js
@@ -2,22 +2,22 @@ export const herodata = [
     {
         id: 1,
         image: "caro",
-        title: "title goes here",
-        description: "description goes here",
+        title: "find your dream rental",
+        description: "browse all our available properties",
         link: "/"
     },
     {
         id: 2,
         image: "caro",
-        title: "title goes here",
-        description: "description goes here",
+        title: "the simple life",
+        description: "lush gardens",
         link: "/"
     },
     {
         id: 3,
         image: "caro",
-        title: "title goes here",
-        description: "description goes here",
+        title: "breathtaking views",
+        description: "beautiful beaches",
         link: "/"
     }
 ]
