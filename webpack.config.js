04a9836e33689e0cee3f3a2f3da12e7d6075c74f
ben sefton <benwpsefton@gmail.com>
2025-11-10 13:45:18 +0800

    reverted to older versions

--- webpack.config.js
+++ webpack.config.js
@@ -3,16 +3,12 @@ const webpack = require("webpack");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const CopyPlugin = require("copy-webpack-plugin");
 
-const isProd = process.env.NODE_ENV === "production";
-const publicPath = isProd ? "/my-react-app/" : "/";
-
 module.exports = {
   entry: "./src/index.js",
   output: {
     filename: "bundle.js",
     path: path.resolve(__dirname, "dist"),
-    publicPath: publicPath,
-    clean: true, // clear dist folder on build
+    publicPath: "/my-react-app/",
   },
   module: {
     rules: [
@@ -29,7 +25,7 @@ module.exports = {
         test: /\.(png|jpg|jpeg|gif|svg)$/i,
         type: "asset/resource",
         generator: {
-          filename: "assets/[name][ext]", // ensure assets are copied to /assets/
+          filename: "assets/[name][ext]",
         },
       },
     ],
@@ -37,16 +33,12 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: "./public/index.html",
-      publicPath: publicPath,
     }),
     new CopyPlugin({
       patterns: [
         { from: "public/assets", to: "assets" },
         { from: "public/index.html", to: "404.html" },
       ],
-    }),
-    new webpack.DefinePlugin({
-      "process.env.PUBLIC_URL": JSON.stringify(publicPath),
     }),
   ],
   devServer: {
