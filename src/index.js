c4c8f2c89120f20fc003682dae3e78510c3de3d5
ben sefton <benwpsefton@gmail.com>
2025-11-10 12:31:11 +0800

    reverted to BrowserRouter

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
 import App from "./App"
-import { HashRouter } from "react-router-dom"
+import { BrowserRouter } from "react-router-dom"
 import "./App.css"
 import "bootstrap/dist/css/bootstrap.min.css"
 
@@ -9,8 +9,8 @@ const root = ReactDOM.createRoot(document.getElementById("root"))
 
 root.render(
     <React.StrictMode>
-        <HashRouter basename={process.env.PUBLIC_URL}>
+        <BrowserRouter basename={process.env.PUBLIC_URL}>
             <App></App>
-        </HashRouter>
+        </BrowserRouter>
     </React.StrictMode>
 )
