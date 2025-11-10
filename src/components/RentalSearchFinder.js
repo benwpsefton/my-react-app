f7c23a93f9aa5d32b5ab7e8a4fd8115b0c90c23c
ben sefton <benwpsefton@gmail.com>
2025-11-10 15:51:12 +0800

    did somnething

--- src/components/RentalSearchFinder.js
+++ src/components/RentalSearchFinder.js
@@ -33,74 +33,66 @@ export default function RentalSearchFinder() {
                     <Form onSubmit={handleSubmit}>
                         <Row>
                             <Col sm={3}>
-                                <Container>
-                                    <InputGroup>
-                                            <Form.Control
-                                                size="sm"
-                                                name="arrival"
-                                                placeholder="Arrive on..."
-                                                type="date"
-                                                data-date-format="DD/MM/YYYY"
-                                                value={formData.arrival}
-                                                onChange={handleChange}
-                                                required
-                                            ></Form.Control>
-                                    </InputGroup>
-                                </Container>
+                                <InputGroup className="d-block">
+                                    <Form.Control
+                                        size="sm"
+                                        name="arrival"
+                                        placeholder="Arrive on..."
+                                        type="date"
+                                        data-date-format="DD/MM/YYYY"
+                                        value={formData.arrival}
+                                        onChange={handleChange}
+                                        required
+                                    ></Form.Control>
+                                </InputGroup>
                             </Col>
                             <Col sm={3}>
-                                <Container>
-                                    <InputGroup>
-                                            <Form.Select
-                                                size="sm"
-                                                name="length"
-                                                value={formData.length}
-                                                onChange={handleChange}
-                                                required
-                                            >
-                                                <option value={""}>Stay...</option>
-                                                {[...Array(14)].map((_, i) => (
-                                                    <option key={i + 1} value={i + 1}>{i + 1} Night{ i + 1 > 1 ? "s" : ""}</option>
-                                                ))}
-                                            </Form.Select>
-                                    </InputGroup>
-                                </Container>
+                                <InputGroup className="d-block">
+                                    <Form.Select
+                                        size="sm"
+                                        name="length"
+                                        value={formData.length}
+                                        onChange={handleChange}
+                                        required
+                                    >
+                                        <option value={""}>Stay...</option>
+                                        {[...Array(14)].map((_, i) => (
+                                            <option key={i + 1} value={i + 1}>{i + 1} Night{ i + 1 > 1 ? "s" : ""}</option>
+                                        ))}
+                                    </Form.Select>
+                                </InputGroup>
                             </Col>
                             <Col sm={3}>
-                                <Container>
-                                    <InputGroup>
-                                            <Form.Select
-                                                size="sm"
-                                                name="bedrooms"
-                                                value={formData.bedrooms}
-                                                onChange={handleChange}
-                                                required
-                                            >
-                                                <option value={""}>Bedrooms</option>
-                                                {[0,1,2,3,4,5, "5plus"].map(val => (
-                                                    <option key={val} value={val}>{val}</option>
-                                                ))}
-                                            </Form.Select>
-                                    </InputGroup>
-                                </Container>
+                                <InputGroup className="d-block">
+                                    <Form.Select
+                                        size="sm"
+                                        name="bedrooms"
+                                        value={formData.bedrooms}
+                                        onChange={handleChange}
+                                        required
+                                    >
+                                        <option value={""}>Bedrooms</option>
+                                        {[0,1,2,3,4,5, "5plus"].map(val => (
+                                            <option key={val} value={val}>{val}</option>
+                                        ))}
+                                    </Form.Select>
+                                </InputGroup>
                             </Col>
                             <Col sm={3}>
-                                <Container>
-                                    <InputGroup>
-                                            <Form.Control
-                                                size="lg"
-                                                type="text"
-                                                name="location"
-                                                value={formData.location}
-                                                onChange={handleChange}
-                                                placeholder="City, State, Country, etc..."
-                                                required
-                                            ></Form.Control>
-                                            <Button type="submit" variant="primary">
-                                                Search
-                                            </Button>
-                                    </InputGroup>   
-                                </Container>
+                                <InputGroup className="d-block">
+                                    <Form.Control
+                                        size="lg"
+                                        type="text"
+                                        name="location"
+                                        value={formData.location}
+                                        onChange={handleChange}
+                                        placeholder="City, State, Country, etc..."
+                                        required
+                                    ></Form.Control>
+                                    <Button type="submit" variant="primary">
+                                        Search
+                                    </Button>
+                                </InputGroup>   
                             </Col>
                         </Row>
                     </Form>
