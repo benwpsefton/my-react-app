75044f237fcae69377b6bfb6f49072891dbbaf96
ben sefton <benwpsefton@gmail.com>
2025-11-10 12:56:29 +0800

    i'm losing my mind

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
   output: {
     filename: "bundle.js",
     path: path.resolve(__dirname, "dist"),
-    publicPath: process.env.PUBLIC_URL || "/",
+    publicPath: process.env.NODE_ENV === "production" ? "/my-react-app/" : "/",
   },
   module: {
     rules: [
