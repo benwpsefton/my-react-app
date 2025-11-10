170eef0b497139b5cf29ce061e346f33174c07a7
ben sefton <benwpsefton@gmail.com>
2025-11-10 12:39:50 +0800

    GetImage verify

--- src/components/GetImage.js
+++ src/components/GetImage.js
@@ -1,3 +1,5 @@
 export default function GetImage(folder, parameter) {
     return `${process.env.PUBLIC_URL}/assets/${folder}/${parameter.image}${parameter.id}.jpg`
+
+    console.log(`${process.env.PUBLIC_URL}/assets/${folder}/${parameter.image}${parameter.id}.jpg`);
 }
