42d8defe56fcc262038ec2130631816913e80026
ben sefton <benwpsefton@gmail.com>
2025-11-10 12:51:05 +0800

    reverted GetImage.js and fixed basename for BrowserRouter

--- src/components/GetImage.js
+++ src/components/GetImage.js
@@ -1,5 +1,3 @@
 export default function GetImage(folder, parameter) {
-    const path = `${process.env.PUBLIC_URL}/assets/${folder}/${parameter.image}${parameter.id}.jpg`;
-    console.log("Image path:", path); // testing
-    return path;
+    return `${process.env.PUBLIC_URL}/assets/${folder}/${parameter.image}${parameter.id}.jpg`
 }
