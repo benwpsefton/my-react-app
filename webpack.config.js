fce24dea993e6d7307fd08eda2b4d4c5125df5a1
ben sefton <benwpsefton@gmail.com>
2025-11-10 13:32:16 +0800

    new webpack

--- webpack.config.js
+++ webpack.config.js
@@ -3,46 +3,41 @@ const webpack = require("webpack");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const CopyPlugin = require("copy-webpack-plugin");
 
+const isProd = process.env.NODE_ENV === "production";
+const publicPath = isProd ? "/my-react-app/" : "/";
+
 module.exports = {
   entry: "./src/index.js",
   output: {
     filename: "bundle.js",
     path: path.resolve(__dirname, "dist"),
-    publicPath: process.env.NODE_ENV === "production" ? "/my-react-app/" : "/",
+    publicPath: publicPath,
+    clean: true, // clear dist folder on build
   },
   module: {
     rules: [
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
-        },
+        use: "babel-loader",
       },
       {
         test: /\.css$/,
-        use: [
-          "style-loader",
-          "css-loader",
-        ],
+        use: ["style-loader", "css-loader"],
       },
       {
         test: /\.(png|jpg|jpeg|gif|svg)$/i,
-        type: 'asset/resource',
-      }
+        type: "asset/resource",
+        generator: {
+          filename: "assets/[name][ext]", // ensure assets are copied to /assets/
+        },
+      },
     ],
   },
   plugins: [
     new HtmlWebpackPlugin({
       template: "./public/index.html",
-      publicPath: process.env.PUBLIC_URL || "/my-react-app/",
-    }),
-    new webpack.DefinePlugin({
-      "process.env.PUBLIC_URL": JSON.stringify(
-        process.env.NODE_ENV === "production"
-          ? "/my-react-app"
-          : ""
-      ),
+      publicPath: publicPath,
     }),
     new CopyPlugin({
       patterns: [
@@ -50,12 +45,12 @@ module.exports = {
         { from: "public/index.html", to: "404.html" },
       ],
     }),
+    new webpack.DefinePlugin({
+      "process.env.PUBLIC_URL": JSON.stringify(publicPath),
+    }),
   ],
   devServer: {
-    static: {
-      directory: path.join(__dirname, "public"),
-    },
-    compress: true,
+    static: path.join(__dirname, "public"),
     historyApiFallback: true,
     port: 3000,
   },
